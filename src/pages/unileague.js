05a76186ba01833a221ebe208925128b85ab2355
Glenn Hansen <hansenglenneirik@gmail.com>
2022-05-25 15:04:42 +0200

    Add scaffolding to projects

--- src/pages/unileague.js
+++ src/pages/unileague.js
@@ -1,13 +1,75 @@
 import React from 'react';
+import ProjectCard from '../components/ProjectCard';
 import ProjectDescription from '../components/ProjectDescription';
 
 import uniLeague from '../images/unileague.jpeg';
+import Plantrol from '../images/plantrol.jpeg';
+import Accessibility from '../images/accessibility.jpeg';
+import BackArrow from '../components/BackArrow';
 
-function UniLeague(){
+function UniLeague() {
     return (
         <main>
+            <BackArrow />
             <h1>uniLeague – Bachelor Project</h1>
             <ProjectDescription title='uniLeague – Bachelor Project' image={uniLeague} github='https://github.com/Bachelorproject-Spring-22' demo='https://unileague.netlify.app/' />
+            <section className='problem-description'>
+                <h2>Project Outline</h2>
+                <h3>Quick Facts <code>= &#123;</code></h3>
+                <ul>
+                    <li><span className='strong'>Timeline:</span> Late December – May 2022</li>
+                    <li><span className='strong'>Type:</span> Group Project</li>
+                    <li><span className='strong'>Course:</span> IDG3920 - Bachelor Project</li>
+                    <li><span className='strong'>Role:</span> Low-fidelity Design, Full-stack Developer</li>
+                    <li><span className='strong'>Tools:</span> Balsamiq, MongoDB Express React Node (MERN)</li>
+                    <li><span className='strong'>Client:</span> Associate Professor at Department of Design at NTNU in Gjøvik</li>
+                </ul>
+
+                <h3>Problem Description</h3>
+                <p>
+                    
+                </p>
+
+                <h3>Our Solution</h3>
+                <p>
+                    
+                </p>
+            </section>
+            <section className='my-work'>
+                <h2>Improving uniLeague</h2>
+                <h3>My role in the project</h3>
+                <article>
+                    <figure>
+                        <img src='https://picsum.photos/500/300' alt='' />
+                    </figure>
+                    <div>
+                        <p>
+                            
+                        </p>
+                    </div>
+                </article>
+
+                <h3>Authentication</h3>
+                <article>
+                    <div>
+                        <p>
+
+                        </p>
+                        <p>
+                            
+                        </p>
+                    </div>
+                    <figure>
+                        <img src='https://picsum.photos/500/300' alt='' />
+                    </figure>
+                </article>
+            </section>
+
+            <h2>Other projects you may like</h2>
+            <section className='projects'>
+                <ProjectCard image={Plantrol} title='Plantrol – Web Project' link='/plantrol' />
+                <ProjectCard image={Accessibility} title='7-Minute-Workout – Accessibility Exam' link='/workout' />
+            </section>
         </main>
     );
 }
--- src/pages/workout.js
+++ src/pages/workout.js
@@ -1,13 +1,74 @@
 import React from 'react';
+import BackArrow from '../components/BackArrow';
+import ProjectCard from '../components/ProjectCard';
 import ProjectDescription from '../components/ProjectDescription';
 
 import Accessibility from '../images/accessibility.jpeg';
+import Plantrol from '../images/plantrol.jpeg';
+import uniLeague from '../images/unileague.jpeg';
 
 function Workout(){
     return (
         <main>
+            <BackArrow />
             <h1>7-Minute-Workout – Accessibility Exam</h1>
             <ProjectDescription image={Accessibility} title='7-Minute-Workout – Accessibility Exam' github='https://github.com/glennispotatis/eksamen-tilgjengelighet-2020' demo='https://folk.ntnu.no/glenneha/idg2012/' />
+            <section className='problem-description'>
+                <h2>Project Outline</h2>
+                <h3>Quick Facts <code>= &#123;</code></h3>
+                <ul>
+                    <li><span className='strong'>Timeline:</span> November – December 2022</li>
+                    <li><span className='strong'>Type:</span> Group Project</li>
+                    <li><span className='strong'>Course:</span> IDG2012 - Accessibility</li>
+                    <li><span className='strong'>Role:</span> Front-end Developer</li>
+                    <li><span className='strong'>Tools:</span> HTML5, CSS3, JavaScript</li>
+                    <li><span className='strong'>Client:</span> Associate Professor at Department of Design at NTNU in Gjøvik</li>
+                </ul>
+                <h3>Problem Description</h3>
+                <p>
+                    
+                </p>
+
+                <h3>Our Solution</h3>
+                <p>
+                    
+                </p>
+            </section>
+            <section className='my-work'>
+                <h2>Improving uniLeague</h2>
+                <h3>My role in the project</h3>
+                <article>
+                    <figure>
+                        <img src='https://picsum.photos/500/300' alt='' />
+                    </figure>
+                    <div>
+                        <p>
+                            
+                        </p>
+                    </div>
+                </article>
+
+                <h3>Authentication</h3>
+                <article>
+                    <div>
+                        <p>
+
+                        </p>
+                        <p>
+                            
+                        </p>
+                    </div>
+                    <figure>
+                        <img src='https://picsum.photos/500/300' alt='' />
+                    </figure>
+                </article>
+            </section>
+
+            <h2>Other projects you may like</h2>
+            <section className='projects'>
+                <ProjectCard image={Plantrol} title='Plantrol – Web Project' link='/plantrol' />
+                <ProjectCard image={uniLeague} title='uniLeague – Bachelor Project' link='/unileague' />
+            </section>
         </main>
     );
 }
