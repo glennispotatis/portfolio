a6b26ed80bebc97fe2ec1309a1376f354826fab7
Glenn Hansen <hansenglenneirik@gmail.com>
2022-05-31 22:42:54 +0200

    Finish uniLeague

--- src/pages/unileague.js
+++ src/pages/unileague.js
@@ -4,6 +4,8 @@ import ProjectDescription from '../components/ProjectDescription';
 
 import uniLeague from '../images/unileague.jpeg';
 import Plantrol from '../images/plantrol.jpeg';
+import uniLeagueResults from '../images/unileague-results.png';
+import uniLeagueCode from '../images/unileague-code.png';
 import Accessibility from '../images/accessibility.jpeg';
 import BackArrow from '../components/BackArrow';
 
@@ -15,14 +17,14 @@ function UniLeague() {
             <ProjectDescription title='uniLeague – Bachelor Project' image={uniLeague} github='https://github.com/Bachelorproject-Spring-22' demo='https://unileague.netlify.app/' />
             <section className='problem-description'>
                 <h2>Project Outline</h2>
-                <h3>Quick Facts = &#123;</h3>
-                <ul>
-                    <li><span className='strong'>Timeline:</span> Late December – May 2022</li>
-                    <li><span className='strong'>Type:</span> Group Project</li>
-                    <li><span className='strong'>Course:</span> IDG3920 - Bachelor Project</li>
-                    <li><span className='strong'>Role:</span> Low-fidelity Design, Full-stack Developer</li>
-                    <li><span className='strong'>Tools:</span> Balsamiq, MongoDB Express React Node (MERN)</li>
-                    <li><span className='strong'>Client:</span> Associate Professor at Department of Design at NTNU in Gjøvik</li>
+                <h3 className='code'>const quickFacts = &#123;</h3>
+                <ul className='code'>
+                    <li>Timeline: Late December – May 2022</li>
+                    <li>Type: Group Project</li>
+                    <li>Course: IDG3920 - Bachelor Project</li>
+                    <li>Role: Low-fidelity Design, Full-stack Developer</li>
+                    <li>Tools: Balsamiq, MongoDB Express React Node (MERN)</li>
+                    <li>Client: Associate Professor at Department of Design at NTNU in Gjøvik</li>
                 </ul>
 
                 <h3>Problem Description</h3>
@@ -45,13 +47,31 @@ function UniLeague() {
                 <h3>My role in the project</h3>
                 <article>
                     <figure>
-                        <img src='https://picsum.photos/500/300' alt='' />
+                        <img src={uniLeagueResults} alt='The resultpage for the course IDG2100 on the application uniLeague' />
                     </figure>
                     <div>
                         <p>
-                            *PLACEHOLDER_TEXT*
+                            In this project, I was responsible for the front-end and connecting the front-end to the back-end. 
+                            I have used HOCs, writing stateless and stateful components, writing logic for logic-based components, and created the user authorization for the front-end by using a context-provider.
                         </p>
                     </div>
+                </article>
+                <h3>Code Optimization</h3>
+                <article>
+                    <div>
+                        <p>
+                            For this project, I would like to optimize the code. 
+                            The whole front-end was created in 2 weeks to make it ready for testing. 
+                            This meant that a lot of shortcuts were taken to be able to finish the project within the timeframe that were set. 
+                            Instead of using ternary operators, I could have used conditional rendering with && operator. 
+                            This would remove a lot of unnecessary “: null” code-snippets. 
+                            Furthermore, the “hide/show password” button in the login-component could be improved. 
+                            As of now it is more or less forced into place and thus it is less “accessible” that it can be.
+                        </p>
+                    </div>
+                    <figure>
+                        <img src={uniLeagueCode} alt='Code showing how to toggle between 2 image states' />
+                    </figure>
                 </article>
             </section>
 
